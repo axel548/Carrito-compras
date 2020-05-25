bc626954e3894ea30f414edd16bafa2aa29048bd
axe <axel.aatl@gmail.com>
2020-05-24 20:03:37 -0600

    Primero SERVER

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,24 @@
 <?php
 //base de datos
+/*
 define("HOST","localhost");
 define("USER","root");
 define("PASS","");
 define("NAME","mvc");
-
-/*
-define("HOST","sql107.tonohost.com");
-define("USER","ottos_25392760");
-define("PASS","katonendan40");
-define("NAME","ottos_25392760_mvc");
 */
+
+define("HOST","server267.web-hosting.com");
+define("USER","axewuhgj");
+define("PASS","7YPuRYiNUJW7");
+define("NAME","axewuhgj_mvc");
+
 //url para entrar a la carpeta app
 define("RUTAAPP",dirname(dirname(__FILE__)));
 
 //url para acceder a la carpeta public
-define("RUTAPUBLIC", "http://localhost/mvc");
+//define("RUTAPUBLIC", "http://localhost/mvc");
+
+define("RUTAPUBLIC", "https://axewes.website/mvc");
 
 define("RUTAIMG", dirname(dirname(dirname(__FILE__))));
 
