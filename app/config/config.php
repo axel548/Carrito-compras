c9cc873d8707f677781df5d16ce4cdb2380610ec
Axel <59571184+axel548@users.noreply.github.com>
2022-03-24 11:14:33 -0400

    Cambio de Constantes Server a Local

--- app/config/config.php
+++ app/config/config.php
@@ -7,10 +7,16 @@ define("PASS","");
 define("NAME","axmvc");
 */
 
-define("HOST","server267.web-hosting.com");
-define("USER","axewuhgj");
-define("PASS","7YPuRYiNUJW7");
-define("NAME","axewuhgj_axmvc");
+//variables server
+// define("HOST","server267.web-hosting.com");
+// define("USER","axewuhgj");
+// define("PASS","7YPuRYiNUJW7");
+// define("NAME","axewuhgj_axmvc");
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("NAME","mvc");
 
 //url para entrar a la carpeta app
 define("RUTAAPP",dirname(dirname(__FILE__)));
@@ -18,7 +24,9 @@ define("RUTAAPP",dirname(dirname(__FILE__)));
 //url para acceder a la carpeta public
 //define("RUTAPUBLIC", "http://localhost/mvc");
 
-define("RUTAPUBLIC", "https://axewes.website/mvc");
+//variables server
+// define("RUTAPUBLIC", "https://axewes.website/mvc");
+define("RUTAPUBLIC", "http://localhost/mvc");
 
 define("RUTAIMG", dirname(dirname(dirname(__FILE__))));
 
