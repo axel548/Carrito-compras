13e5ac37cf7fe0d49db47bc3a200ebb85b4cc0aa
axel <axel.aatl@gmail.com>
2022-03-23 19:43:15 -0600

    Mejoras

--- app/config/config.php
+++ app/config/config.php
@@ -1,24 +1,24 @@
 <?php
 //base de datos
-
+/*
 define("HOST","localhost");
 define("USER","root");
 define("PASS","");
 define("NAME","axmvc");
+*/
 
-/*
 define("HOST","server267.web-hosting.com");
 define("USER","axewuhgj");
 define("PASS","7YPuRYiNUJW7");
-define("NAME","axewuhgj_mvc");
-*/
+define("NAME","axewuhgj_axmvc");
+
 //url para entrar a la carpeta app
 define("RUTAAPP",dirname(dirname(__FILE__)));
 
 //url para acceder a la carpeta public
-define("RUTAPUBLIC", "http://localhost/mvc");
+//define("RUTAPUBLIC", "http://localhost/mvc");
 
-//define("RUTAPUBLIC", "https://axewes.website/mvc");
+define("RUTAPUBLIC", "https://axewes.website/mvc");
 
 define("RUTAIMG", dirname(dirname(dirname(__FILE__))));
 
